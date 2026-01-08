79a4307d35db484b47a805d4c3e38383a6a4576f
pallavi dannur <pallavidannur96@gmail.com>
2026-01-08 11:52:01 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,25 @@
+first_name = "rahul"
+last_name = "dannur"
+full_name = first_name + " " + last_name
+print(full_name)
+
+message = "this is Warning !"
+print(message*5)
+
+print(message.upper())
+print(message.lower()) 
+print(message.strip()) 
+print(message.replace("Warning", "error"))
+
+name = 'pallavi said "hello" '
+print(name)
+name = "pallavi said 'hello'"
+print(name)
+name = '''pallavi said "hello"
+          she said "how are you" '''
+print(name)
+name ="pallavi"
+print(name[5])
+print(name[0:7])
+s = "pallavi \n is a good girl"
+print(s)
