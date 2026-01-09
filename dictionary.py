df5a195774b817002f0c7e14166985d65f325a38
pallavi dannur <pallavidannur96@gmail.com>
2026-01-09 11:52:49 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+birthday = {
+    "pallavi": "03-08-2006",
+    "rahul" : "15-12-2007",
+    "rohit" : "01-01-2008"
+}
+
+meanings = {
+    "pallavi": "new leaves",
+    "rahul" : "conqueror of all miseries",
+    }
+print(birthday["pallavi"])
+print(meanings)
