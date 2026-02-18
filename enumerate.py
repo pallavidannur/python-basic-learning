bd31e887eb2d144da1c4c7239467532ec76a79b7
pallavi dannur <pallavidannur96@gmail.com>
2026-02-18 23:30:34 +0530

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,15 @@
+name = "pallavi"
+for index, letter in enumerate(name):
+    print(letter*(index+1))
+
+
+
+
+    i = 12,23,34
+
+    for num in i:
+        print(num)
+        if num == 23:
+            break
+        else:
+            print("all found")
