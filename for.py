662ef381aea9bda68d4c4e7608ef268c08d381e4
pallavi dannur <pallavidannur96@gmail.com>
2026-02-18 22:58:11 +0530

    Add files via upload

--- for.py
+++ for.py
@@ -1,3 +1,17 @@
 cities = ["bangaluru,mysuru,huballi,mangaluru"]
 for city in cities:
     print(city)
+
+
+
+    bag = ["red","yellow"," pink"]
+for color in bag:
+    print(color)
+
+    for i in range(1,11,2):
+        print(i)
+
+        name = "pallavi" 
+
+        for letter in name:
+            print(letter*3)
