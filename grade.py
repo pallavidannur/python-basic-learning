4d433aa1facd0f12ad286b9572f1073167d09d1e
pallavi dannur <pallavidannur96@gmail.com>
2026-02-19 10:26:00 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,8 @@
+student = ["pallavi","keertan","raju"]
+marks = [85,90,78]
+
+student_marks = {}
+
+for i in range( 1,len(student)):
+    student_marks[student[i]] = marks[0]
+    print(student_marks)
