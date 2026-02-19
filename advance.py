15a4dceae8b308f09d97090556bb025758613a7c
pallavi dannur <pallavidannur96@gmail.com>
2026-02-19 10:27:28 +0530

    Add files via upload

---
+++ advance.py
@@ -0,0 +1,14 @@
+l = [1,23,43,534,32]
+total = 0
+for num in l:
+    total = total + num
+
+    print(total)
+
+
+    i = [1,23,43,534,32]
+    dl = []
+
+    for num in i:
+        dl.append(num*2)
+        print(dl)
