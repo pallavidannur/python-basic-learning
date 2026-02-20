3dae6199c9157cb3082ca8163000c978baab460c
pallavi dannur <pallavidannur96@gmail.com>
2026-02-20 11:18:05 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,9 @@
+def marraige(boy,girl):
+  print(f"Boy is {boy}")
+  print(f"girl is {girl}")
+  print(f"{boy} married {girl}")
+
+  marriage ("chandan", "chanadna")
+  marriage ("santosh", "pavitra")
+
+
