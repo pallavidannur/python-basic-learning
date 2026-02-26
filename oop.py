4503750c86e85126c8644ac8892b9f36c4151f0a
pallavi dannur <pallavidannur96@gmail.com>
2026-02-26 10:48:09 +0530

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,12 @@
+class human:
+    def __init__(self, name):
+        self.name = name
+
+    def walk(self):
+        print(f"{self.name} is walking.")
+
+        pallavi = human("Pallavi")
+        keeratna = human("Keeratna")
+       
+       
+        pallavi.walk()
